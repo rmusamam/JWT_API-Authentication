738f7961881c07768cfb683193dfe51f8b1411c1
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-21 22:09:03 +0500

    Create the record from posted data in Register and Saved in DB successfully

--- Route/auth.js
+++ Route/auth.js
@@ -1,10 +1,20 @@
-const router = require('express').Router();
- 
+const router = require("express").Router();
+const user = require("../Model/user");
 
-router.post('/register',(req,res)=>{
-    console.log(req)
-    console.log("Request Body:",req.body);
-     res.send("register")
-})
+router.post("/register", async (req, res) => {
+  const userAdd =  await new user({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+     const saveUser = await userAdd.save();
+    // console.log("User Saved", saveUser);
+    // res.send(saveUser);
+    res.send(saveUser)
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-module.exports=router;  
+module.exports = router;
--- index.js
+++ index.js
@@ -4,22 +4,25 @@ const mongoose= require('mongoose')
 const dotenv=require('dotenv')
 dotenv.config()
 
-
-
-// url='mongodb+srv://rmusamam:<password>@cluster0.fsilfnm.mongodb.net/?API-CRUD'
-url=process.env.DB_CONNECT;
-
-console.log(url)
-mongoose.connect(url,{
-    useNewUrlParser:true
-},()=>{console.log('Data Base Connected')})
-
-
 //Import Routes
 const authRoute= require('./Route/auth')
 
+url=process.env.DB_CONNECT;
+
+mongoose.connect(url,{
+    useNewUrlParser:true
+},
+()=>{
+    console.log('Data Base Connected')
+})
+
+
+//MIddleware
+app.use(express.json())
+
 //Route Middleware
 app.use('/api/user',authRoute)
+
 
 
 app.listen(3000,()=>{
