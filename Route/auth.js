dce5c55d8e6f53604aea5ccac0629f5f84a633b0
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-21 22:30:47 +0500

    joi.validate is creating issue

--- Route/auth.js
+++ Route/auth.js
@@ -1,20 +1,40 @@
 const router = require("express").Router();
 const user = require("../Model/user");
+const joi= require("@hapi/joi")
+
+const schema={
+    name:joi.string()
+    .min(5)
+    .required(),
+    email:joi.string()
+    .min(5)
+    .required(),
+    password:joi.string()
+    .min(5)
+    .required()
+}
+
 
 router.post("/register", async (req, res) => {
-  const userAdd =  await new user({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  try {
-     const saveUser = await userAdd.save();
-    // console.log("User Saved", saveUser);
-    // res.send(saveUser);
-    res.send(saveUser)
-  } catch (err) {
-    res.status(400).send(err);
-  }
+
+//lets validate the data before we add a user in DB
+
+const validate=schema.validate(req.body)
+res.send(validate)
+
+//   const userAdd =  await new user({
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password,
+//   });
+//   try {
+//      const saveUser = await userAdd.save();
+//     // console.log("User Saved", saveUser);
+//     // res.send(saveUser);
+//     res.send(saveUser)
+//   } catch (err) {
+//     res.status(400).send(err);
+//   }
 });
 
 module.exports = router;
