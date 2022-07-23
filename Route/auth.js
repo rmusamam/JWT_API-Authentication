f8e4385d7d092b731f07ebd9038addcb78fad618
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-23 21:19:39 +0500

    hashing done but facing error of encrypting User password Promise is not working

--- Route/auth.js
+++ Route/auth.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const user = require("../Model/user");
 const bcryptjs=require('bcryptjs')
-const { registerValidation } = require("../validation");
+const { registerValidation,loginValidation } = require("../validation");
 
+
+//ROUTER OF REGISTER PAGE
 router.post("/register", async (req, res) => {
-  //lets validate the data before we add a user in DB
 
+
+  //lets validate the data before we add a user in DB
   const { error } = registerValidation(req.body);
   console.log(error, "this is error");
 
@@ -15,28 +18,69 @@ router.post("/register", async (req, res) => {
     ("user already exists");
     res.status(400).send("Email already exists");
   }
-
+  //IF THERE IS AN ERROR OF TYPO 
   if (error) {
     sendError = res.status(400).send(error.details[0].message);
     return sendError;
   }
-
+  // DECRYPTING THE PASSWORD
   const salt= await bcryptjs.genSalt(10)
   const hashedPassword= await bcryptjs.hash(req.body.password,salt)
 
-  console.log("if not executed");
+  //ADDING NEW USER IN DB
   const userAdd = await new user({
     name: req.body.name,
     email: req.body.email,
     password: hashedPassword
   });
   try {
+    // USER ADDED
     const saveUser = await userAdd.save();
     console.log("User Saved", saveUser);
     res.send(saveUser);
   } catch (err) {
+    //USER CAN'T BE ADDED
     res.status(400).send(err);
   }
 });
+
+
+//ROUTER OF LOGIN
+router.post('/login',async(req,res)=>{
+    try{
+
+
+    //VALIDATE THE USER 
+    console.log("enter in login")
+    const {error}= loginValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+
+    console.log("checking email")
+
+    //EMAIT NOT EXIST
+    const validUser= await user.findOne({email: req.body.email})
+    if(!validUser) return res.status(400).send("The Email does not exist")
+
+
+    console.log("checking password")
+
+
+    //PASSWORD IS WRONG
+    console.log(req.body)
+    console.log(user.password)
+    const validPassword= await bcryptjs.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(400).send("The Password is wrong")
+
+    console.log("LOGIN")
+
+
+    res.send('Login Successfully')
+}catch(err){
+    console.log(err)
+}
+
+})
+
 
 module.exports = router;
