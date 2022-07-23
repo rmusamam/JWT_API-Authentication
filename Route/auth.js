36d7a5e0d0258d40f90a897069a210512b06c66d
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-23 16:44:41 +0500

    validate part has been done

--- Route/auth.js
+++ Route/auth.js
@@ -19,22 +19,30 @@ router.post("/register", async (req, res) => {
 
 //lets validate the data before we add a user in DB
 
-const validate=schema.validate(req.body)
-res.send(validate)
+const {error}=schema.validate(req.body)
 
-//   const userAdd =  await new user({
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
-//   try {
-//      const saveUser = await userAdd.save();
-//     // console.log("User Saved", saveUser);
-//     // res.send(saveUser);
-//     res.send(saveUser)
-//   } catch (err) {
-//     res.status(400).send(err);
-//   }
+// Testing the response
+// res.send(error)
+// console.log(error.details[0].message)
+
+if(error){
+    sendError=res.status(400).send(error.details[0].message);
+    return sendError
+}
+
+  const userAdd =  await new user({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+     const saveUser = await userAdd.save();
+    // console.log("User Saved", saveUser);
+    // res.send(saveUser);
+    res.send(saveUser)
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
