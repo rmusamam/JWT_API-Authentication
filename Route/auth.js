357dfc47059c9e2ad9b3b3f6ed5c683d80439eac
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-23 20:00:34 +0500

    Password decrypted by Using bcryptjs Library, Used salt and hash functions

--- Route/auth.js
+++ Route/auth.js
@@ -1,42 +1,39 @@
 const router = require("express").Router();
 const user = require("../Model/user");
-const{registerValidation}=require('../validation')
+const bcryptjs=require('bcryptjs')
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  //lets validate the data before we add a user in DB
 
-//lets validate the data before we add a user in DB
+  const { error } = registerValidation(req.body);
+  console.log(error, "this is error");
 
-// const {error}=schema.validate(req.body)
-const {error}=registerValidation(req.body)
-console.log(error,'this is error')
-// Testing the response
-// res.send(error)
-// console.log(error.details[0].message)
+  //CHECKING FOR ALREADY USED EMAIL
+  const userExist = await user.findOne({ email: req.body.email });
+  if (userExist) {
+    ("user already exists");
+    res.status(400).send("Email already exists");
+  }
 
-//CHECKING FOR ALREADY USED EMAIL
-const userExist= await user.findOne({email: req.body.email})
-if(userExist){
-    "user already exists"
-    res.status(400).send("Email already exists")
-}
+  if (error) {
+    sendError = res.status(400).send(error.details[0].message);
+    return sendError;
+  }
 
+  const salt= await bcryptjs.genSalt(10)
+  const hashedPassword= await bcryptjs.hash(req.body.password,salt)
 
-if(error){
-    sendError=res.status(400).send(error.details[0].message);
-    return sendError
-}
-
-    console.log('if not executed')
-    const userAdd =  await new user({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+  console.log("if not executed");
+  const userAdd = await new user({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
   });
   try {
-     const saveUser = await userAdd.save();
-     console.log("User Saved", saveUser);
-    // res.send(saveUser);
-    res.send(saveUser)
+    const saveUser = await userAdd.save();
+    console.log("User Saved", saveUser);
+    res.send(saveUser);
   } catch (err) {
     res.status(400).send(err);
   }
